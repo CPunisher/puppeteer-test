cd60c51500fce4065177519d4dd1a3d7f79c1bb2
CPunisher <1343316114@qq.com>
2022-11-17 14:56:24 +0800

    feat: url parser utils

--- scripts/performance.js
+++ scripts/performance.js
@@ -6,7 +6,7 @@ import puppeteer from "puppeteer";
 program
   .argument('<string>', 'Target url')
   .option('--interval <number>', 'Monitor interval in ms', 1000)
-  .option('--duration <number>', 'Monitor duration in ms', 3000)
+  .option('--duration <number>', 'Monitor duration in ms', 20000)
   .parse();
 
 const url = program.args[0];
@@ -14,18 +14,21 @@ const { interval, duration } = program.opts();
 (async () => { 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  page.goto(url);
+  const pid = browser.process().pid;
+  await page.goto(url);
+
   const monitor = setInterval(async () => {
-    const pid = browser.process().pid;
-    const pidList = [pid, ...await pidtree(pid)];
-    const infoList = await Promise.all(pidList.map(ch => pidusage(ch)));
-    const totalMemory = infoList.reduce((a, b) => a + b.memory, 0);
-    console.log(pidList, (totalMemory / 1024 / 1024).toFixed(2) + "MB");
+    if (!page.isClosed()) {
+      const pidList = [pid, ...await pidtree(pid)];
+      const infoList = await Promise.all(pidList.map(ch => pidusage(ch)));
+      const totalMemory = infoList.reduce((a, b) => a + b.memory, 0);
+      console.log(pidList, (totalMemory / 1024 / 1024).toFixed(2) + "MB");
+    }
   }, interval);
   
-  setTimeout(() => {
-    clearTimeout(monitor);
-    page.close();
-    browser.close();
+  setTimeout(async () => {
+    clearInterval(monitor);
+    await page.close();
+    await browser.close();
   }, duration);
 })();
